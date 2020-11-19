17eca8fae1f29321cfb5b28c679da509f58f27a2
7mming7 <7mming7@gmail.com>
2020-11-19 10:34:55 +0800

    Add the alias function week of weekofyear

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
@@ -637,6 +637,7 @@ object FunctionRegistry {
     expression[DayOfWeek]("dayofweek"),
     expression[WeekDay]("weekday"),
     expression[WeekOfYear]("weekofyear"),
+    expression[WeekOfYear]("week"),
     expression[Year]("year"),
     expression[TimeWindow]("window"),
     expression[SessionWindow]("session_window"),
