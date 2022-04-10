acc1b3c1d91efb6dc5144e3e84bfb209c462b431
zhen.song <2438143730@qq.com>
2022-04-10 09:52:36 +0800

    window funnel fix

--- sql/core/src/test/scala/org/apache/spark/sql/TypedImperativeAggregateSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/TypedImperativeAggregateSuite.scala
@@ -646,6 +646,44 @@ class TypedImperativeAggregateSuite extends QueryTest with SharedSparkSession {
     ), Seq ((901, 5), (902, 2) ) )
   }
 
+  ignore ("test window funnel 002") {
+    val colNames = Seq ("lstg_format_name", "cal_dt", "lstg_site_id",
+      "seller_id", "trans_id")
+
+    val df1 = Seq (
+      ("ABIN", "2012-01-01", 0, 10000294, 0)
+      , ("FP-non GTC", "2012-01-01", 0, 10000294, 1)
+      , ("Others", "2012-01-01", 0, 10000294, 2)
+      , ("Auction", "2012-01-01", 0, 10000294, 3)
+      , ("ABIN", "2012-01-01", 1, 10000294, 0)
+      , ("FP-non GTC", "2012-01-01", 1, 10000294, 1)
+      , ("Others", "2012-01-01", 1, 10000294, 2)
+
+    ).toDF (colNames: _*)
+    df1.createOrReplaceTempView ("test_kylin_fact")
+    checkWindowAnswer (spark.sql (
+      "select seller_id," +
+        " window_funnel(\n 5 * 24 * 60 * 60, \n" +
+        " 4, \n" +
+        " cal_dt, \n" +
+        " case when lstg_format_name = 'ABIN' or lstg_format_name = 'FP-non GTC' then '0,1'\n" +
+        " when lstg_format_name = 'Others' then 2\n" +
+        " when lstg_format_name = 'Auction' then 3\n" +
+        " else -1 end, \n" +
+        " case when lstg_format_name = 'ABIN' then lstg_site_id\n" +
+        " when lstg_format_name = 'FP-non GTC' then lstg_site_id\n" +
+        " when lstg_format_name = 'Others' then lstg_site_id\n" +
+        " when lstg_format_name = 'Auction' then lstg_site_id\n" +
+        " else null end,\n" +
+        " struct(struct(1, seller_id), struct(3, trans_id)) \n ) seq\n" +
+        "from test_kylin_fact \n\n" +
+        "where cal_dt >= '2012-01-01' and cal_dt < '2012-01-02' \n" +
+        "group by seller_id \n" +
+        "order by seq desc\n" +
+        "LIMIT 500"
+    ), Seq ((901, 5), (902, 2) ) )
+  }
+
   ignore ("test window funnel 001") {
     val colNames = Seq ("lstg_format_name", "cal_dt", "lstg_site_id",
       "seller_id", "trans_id")
