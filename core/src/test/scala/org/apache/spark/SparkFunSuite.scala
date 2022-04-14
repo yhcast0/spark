729bbe21df53219b2993570fd7beeaef38f1d0a1
jiawei.li <1019037687@qq.com>
2022-04-14 15:29:07 +0800

    fix sparkFunSuite LogAppender

--- core/src/test/scala/org/apache/spark/SparkFunSuite.scala
+++ core/src/test/scala/org/apache/spark/SparkFunSuite.scala
@@ -256,23 +256,30 @@ abstract class SparkFunSuite
   }
 
   class LogAppender(msg: String = "", maxEvents: Int = 1000)
-      extends AbstractAppender("logAppender", null, null) {
-    val loggingEvents = new ArrayBuffer[LogEvent]()
+    extends AbstractAppender("logAppender", null, null) {
+    private val _loggingEvents = new ArrayBuffer[LogEvent]()
     private var _threshold: Level = Level.INFO
 
-    override def append(loggingEvent: LogEvent): Unit = {
-      if (loggingEvent.getLevel.isMoreSpecificThan(_threshold)) {
-        if (loggingEvents.size >= maxEvents) {
-          val loggingInfo = if (msg == "") "." else s" while logging $msg."
-          throw new IllegalStateException(
-            s"Number of events reached the limit of $maxEvents$loggingInfo")
+    override def append(loggingEvent: LogEvent): Unit = loggingEvent.synchronized {
+      val copyEvent = loggingEvent.toImmutable
+      if (copyEvent.getLevel.isMoreSpecificThan(_threshold)) {
+        _loggingEvents.synchronized {
+          if (_loggingEvents.size >= maxEvents) {
+            val loggingInfo = if (msg == "") "." else s" while logging $msg."
+            throw new IllegalStateException(
+              s"Number of events reached the limit of $maxEvents$loggingInfo")
+          }
+          _loggingEvents.append(copyEvent)
         }
-        loggingEvents.append(loggingEvent)
       }
     }
 
     def setThreshold(threshold: Level): Unit = {
       _threshold = threshold
+    }
+
+    def loggingEvents: ArrayBuffer[LogEvent] = _loggingEvents.synchronized {
+      _loggingEvents.filterNot(_ == null)
     }
   }
 }
