42326cf49dc60c055a921bb350ff32e2207915aa
Yinghao Lin <yinghao.lin@kyligence.io>
2023-11-27 20:40:03 +0800

    Revert "dgbank: support varchar2 data type"
    This reverts commit e89d86e1aba2f7c642d65b24518c1c1638d8ed6a.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -1025,19 +1025,13 @@ private[hive] object HiveClientImpl extends Logging {
     } else {
       CharVarcharUtils.getRawTypeString(c.metadata).getOrElse(c.dataType.catalogString)
     }
-//    new FieldSchema(c.name, typeString, c.getComment().orNull)
-    val fieldSchema = new FieldSchema()
-    fieldSchema.setName(c.name)
-    fieldSchema.setType(typeString)
-    fieldSchema.setComment(c.getComment().orNull)
-    fieldSchema
+    new FieldSchema(c.name, typeString, c.getComment().orNull)
   }
 
   /** Get the Spark SQL native DataType from Hive's FieldSchema. */
   private def getSparkSQLDataType(hc: FieldSchema): DataType = {
     try {
-      val dataType = hc.getType.replaceAll(",ORACLE", "")
-      CatalystSqlParser.parseDataType(dataType)
+      CatalystSqlParser.parseDataType(hc.getType)
     } catch {
       case e: ParseException =>
         throw QueryExecutionErrors.cannotRecognizeHiveTypeError(e, hc.getType, hc.getName)
