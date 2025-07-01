0aac20f1ea4b5ddda57e9a65155f252c0333e192
Zhichao Zhang <441586683@qq.com>
2025-07-01 15:01:58 +0800

    KE-36161 fix replace function ut (#818)
    KE-36161 fix replace function ut

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/StringExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/StringExpressionsSuite.scala
@@ -440,7 +440,7 @@ class StringExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper {
     checkEvaluation(
       StringReplace(Literal("replace"), Literal("pl"), Literal("123")), "re123ace")
     checkEvaluation(StringReplace(Literal("replace"), Literal("pl"), Literal("")), "reace")
-    checkEvaluation(StringReplace(Literal("replace"), Literal(""), Literal("123")), "replace")
+    checkEvaluation(StringReplace(Literal("replace"), Literal(""), Literal("123")), "123r123e123p123l123a123c123e123")
     checkEvaluation(StringReplace(Literal.create(null, StringType),
       Literal("pl"), Literal("123")), null)
     checkEvaluation(StringReplace(Literal("replace"),
