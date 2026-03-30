593a4ab7b188bd167372066a1cc9281ded30abab
Yinghao Lin <yhcast0@apache.org>
2026-03-30 17:34:24 +0800

    KE-45089 Fix manual broadcast cleaning leaks (#875)
    * KE-45089 Fix manual broadcast cleaning leaks
    
    * KE-45089 [FOLLOWUP] Add try catch block for cleanBroadcast & keepBroadcast

--- sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
@@ -963,6 +963,7 @@ object SparkSession extends Logging {
         setDefaultSession(session)
         setActiveSession(session)
         registerContextListener(sparkContext)
+        extraInit()
       }
 
       return session
@@ -1114,6 +1115,11 @@ object SparkSession extends Logging {
       })
       newSession
     }
+  }
+
+  /** Do extra init */
+  def extraInit(): Unit = {
+    SQLExecution.startOutdatedBroadcastCleaner()
   }
 
   ////////////////////////////////////////////////////////////////////////////////////////
